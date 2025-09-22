f9a1bc030d6aaef89b039f7bd1a94b9358482a4b
Matias Jimenez <matf.jimenezc@gmail.com>
2025-09-22 12:23:24 -0300

    Todo

--- Home/Home.jsx
+++ Home/Home.jsx
@@ -1,13 +1,21 @@
+import Footer from "../React/src/Components/Footer"
 import Navbar from "../React/src/Components/Navbar"
+import Cta from "./Components/Cta"
 import Hero from "./Components/Hero"
+import Instituciones from "./Components/Instituciones"
 
 
 function Home() {
     return(
         <>
         <Navbar/>
-        <Hero/> 
+        <Hero/>
+        <Instituciones/>
+        <Cta/>
+        <Footer/>
         </> 
 
     )
 }
+
+export default Home
