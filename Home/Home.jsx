e363a81f2441b972e0943c0cfab49c46f1b948f7
Matias Jimenez <matf.jimenezc@gmail.com>
2025-09-22 11:50:36 -0300

    React Home

---
+++ Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../React/src/Components/Navbar"
+import Hero from "./Components/Hero"
+
+
+function Home() {
+    return(
+        <>
+        <Navbar/>
+        <Hero/> 
+        </> 
+
+    )
+}
