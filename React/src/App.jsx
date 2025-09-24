22271a5e47d04a8d909e089b9172aae1b0685945
Matias Jimenez <matf.jimenezc@gmail.com>
2025-09-24 12:28:55 -0300

    Login + Resgister

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,12 +4,15 @@ import Home from '../../Home/Home'
 import DetalleInstituto from '../../Home/Components/DetalleInstituto'
 import '../../Home/stylehome.css'
 
+
+
 function App() {
   
 
   return (
     <Routes>
     <Route path="/"element={<Home/>} />
+    
     <Route path="/datalle/:id" element={<DetalleInstituto/>} />
      
     </Routes>
