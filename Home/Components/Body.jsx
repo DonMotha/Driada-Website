94ccdcaad88b58d26e6b8dd8975c7b99e1be45f9
Valentina1891 <vvaldiviam@uft.edu>
2025-09-25 17:04:51 -0300

    Guardando cambios en Body.jsx antes de cambiar de rama

--- Home/Components/Body.jsx
+++ Home/Components/Body.jsx
@@ -20,23 +20,3 @@ function Body() {
 export default Body
 
 
-import Footer from "../../React/src/Components/Footer"
-import Navbar from "../../React/src/Components/Navbar"
-import Apoyo from "./Apoyo"
-import Hero from "./Hero"
-import Instituciones from "./Instituciones"
-
-
-function Body() {
-    return(
-        <div className="bg-light">
-            <Navbar/>
-            <Hero/>
-            <Instituciones/>
-            <Apoyo/>
-            <Footer/>
-        </div>
-    )
-}
-
-export default Body
