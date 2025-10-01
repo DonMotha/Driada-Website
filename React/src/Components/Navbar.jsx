3fd783497a49be476d128d4d6e661a219e3c414f
Matias Jimenez <matf.jimenezc@gmail.com>
2025-10-01 15:48:42 -0300

    detalles

--- React/src/Components/Navbar.jsx
+++ React/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../public/apple-touch-icon.png";
 
+
 function Navbar() {
   const navigate = useNavigate();
   const go = (e, to) => {
