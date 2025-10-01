34cf2773c324e72ca207a160592320a86cfa66ea
Matias Jimenez <matf.jimenezc@gmail.com>
2025-10-01 16:38:10 -0300

    Mover comps Carreras

---
+++ React/src/Components/Carreras.comps/Carreras.jsx
@@ -0,0 +1,47 @@
+import { useParams } from "react-router-dom";
+import { CARRERAS } from "./CarrerasA";
+import '../../../../Carreras/Carreras.css';
+
+function Carreras() {
+  const { id } = useParams(); // obtiene el id desde la URL
+  const carrera = CARRERAS.find((c) => c.id === id);
+
+  if (!carrera) return <p className="text-center">Carrera no encontrada</p>;
+
+  return (
+    <div className="carreras-container">
+      <div className="image-placeholder">
+        <img
+          src={carrera.img}
+          alt={carrera.nombre}
+          className="img-fluid"
+          style={{ borderRadius: "8px" }}
+        />
+      </div>
+
+      <div className="subtitle">{carrera.tipo}</div>
+      <h1>{carrera.nombre}</h1>
+
+      <div className="tags">
+        {carrera.tags.map((t, idx) => (
+          <div key={idx} className="tag">
+            {t}
+          </div>
+        ))}
+      </div>
+
+      <div className="rating">
+        ✩ <span>{carrera.rating}</span> ({carrera.votos}% votos)
+      </div>
+
+      <div className="section-title">Qué te puede atraer de esta Carrera</div>
+      <p>{carrera.descripcion}</p>
+
+      <a href="#" className="button">
+        Ver Malla
+      </a>
+    </div>
+  );
+}
+
+export default Carreras;
