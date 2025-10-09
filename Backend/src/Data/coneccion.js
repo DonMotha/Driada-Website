90a688cdcdd27df3ff5c00636746867c2fa6a903
Matias Jimenez <matf.jimenezc@gmail.com>
2025-10-09 10:26:18 -0300

    UpdateModeloBecas

--- Backend/src/Data/coneccion.js
+++ Backend/src/Data/coneccion.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const dbConneccion = async ()=>{
-    try{
-       await  mongoose.connect("mongodb+srv://valentinavaldiviame:Driada12345@cluster0.9woi1em.mongodb.net/Driada?retryWrites=true&w=majority");
-       console.log("Conexión exitosa a MongoDb")
-    }catch(error){
+const dbConneccion = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://valentinavaldiviame:Driada12345@cluster0.9woi1em.mongodb.net/Driada?retryWrites=true&w=majority");
+        console.log("Conexión exitosa a MongoDb")
+    } catch (error) {
         throw new Error("Error a la hora de iniciar la base de datos");
     }
 };
 
-module.exports = {dbConneccion};
+module.exports = { dbConneccion };
--- Backend/src/Models/BecasModel.js
+++ Backend/src/Models/BecasModel.js
@@ -4,20 +4,20 @@ const { Schema , model} = require("mongoose");
 const becaSchema = new Schema(
     {
         nombre: { type: String, required: true },
-        institutionId: { type: mongoose.Schema.Types.ObjectId, ref: "Institucion", required: true },
+        institutionId: { type: Schema.Types.ObjectId, ref: "Institucion", required: true },
         areas: { type: String },            // "Educación Superior"
         activa: { type: Boolean, default: true },
         tipo: { type: String },             // "Beca"
         modalidad: { type: String },        // "Presencial"
         requisitos: {
-            type: mongoose.Schema.Types.Mixed // deja flexible (RSH, puntaje, etc.)
+            type: Schema.Types.Mixed // deja flexible (RSH, puntaje, etc.)
         },
         duracion: { type: String },         // "Carrera"
-        fechas: { type: mongoose.Schema.Types.Mixed }, // { inicio, fin } u otro shape
+        fechas: { type: Schema.Types.Mixed }, // { inicio, fin } u otro shape
         link: { type: String }
     },
     { collection: "Becas", timestamps: true }
 );
 
-const Beca = model("Beca", becaSchema, "Becas");
-module.exports = { Beca };
+
+module.exports = model("Beca", becaSchema, "Becas");
