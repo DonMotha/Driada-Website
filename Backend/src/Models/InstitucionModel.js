b70b51a9e00cd50e0ab80a1e6314996ab23b21f1
Valentina1891 <vvaldiviam@uft.edu>
2025-10-09 09:26:55 -0300

    Arreglado error de tipeo

--- Backend/src/Models/InstitucionModel.js
+++ Backend/src/Models/InstitucionModel.js
@@ -1,14 +1,14 @@
 const {Schema,model} = require("mongoose");
 
 const institucionSchema=new Schema({
-  Nommbre: { type: String, required: true },
+  Nombre: { type: String, required: true },
   Tipo: { type: String, require: true },
   Localidad: {type:String, require:true},
   Description:{type:String, require:true},
   link:{type:String, require:true},
   img:{type:String, require:true},
   Puntuacion:{type:Number,require:true},
-  CantOpiniones:{type:Number}
+  CantiOpiniones:{type:Number}
 })
 ;
 
